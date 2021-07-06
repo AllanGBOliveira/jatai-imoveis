5234fd2c6a2a82bc018e7fff855a565505f5159c
AllanGBOliveira <allebeira@gmail.com>
2021-07-06 11:23:09 -0300

    page added

--- page.php
+++ page.php
@@ -1,119 +1,175 @@
 <?php include('header.php'); ?>
-
-	<main id="page" tabindex="-1">
-		<div class="container">
-			<section>
-				<h1 class="title">Titulo da Página</h1>
-			</section>
-			<section>
-				<nav aria-label="breadcrumb">
-					<ol class="breadcrumb">
-						<li class="breadcrumb-item"><a href="#">Home</a></li>
-						<li class="breadcrumb-item active" aria-current="page">Page</li>
-					</ol>
-				</nav>
-			</section>
-			<section class="page">
-				<article>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit facere quia ad maxime, illo ut, facilis aliquam cumque accusamus maiores consectetur earum nobis dolorem neque culpa sint? Amet iusto, quae! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci repellat aspernatur, quisquam quas vel sunt deserunt, nostrum error soluta dicta dignissimos laborum placeat quaerat, ipsa labore ipsam. Odit, fugit officiis.</p>
-					<br>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, nam cupiditate tempore ipsam asperiores quisquam dolorem quaerat iusto dicta, facere quis porro, voluptatem accusantium consequuntur modi repudiandae illo inventore quas.</p>
-					<br>
-					<h2>Exemplos:</h2>
-					<br>
-					<h1>Texto em h1</h1>
-					<h2>Texto em h2</h2>
-					<h3>Texto em h3</h3>
-					<h4>Texto em h4</h4>
-					<h5>Texto em h5</h5>
-					<h6>Texto em h6</h6>
-					<p><a href="#">Link de teste</a></p>
-					<p><strong>Texto em negrito</strong></p>
-					<p><em>Texto em itálico</em></p>
-					<p><del>Texto riscado</del></p>
-					<ul>
-						<li>1</li>
-						<li>2</li>
-						<li>3</li>
-					</ul>
-					<ol>
-						<li>1</li>
-						<li>2</li>
-						<li>3</li>
-					</ol>
-					<p><blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita perferendis porro aliquam dolorem inventore fuga deleniti corporis quaerat quis vero, recusandae laboriosam qui maiores blanditiis minus corrupti ipsum possimus accusantium..</blockquote></p>
-					<div class="gallery-content">
-						<div class="row row-cols-2 row-cols-sm-3 row-cols-lg-4">		
-							<div class="item col">
-								<button data-src="imgs/default.png" data-fancybox="gallery">
-									<figure>
-										<img class="img-cover lazyload" data-src="imgs/default.png" alt="Imagem 01">
-									</figure>
-								</button>
-							</div>
-							<div class="item col">
-								<button data-src="imgs/default.png" data-fancybox="gallery">
-									<figure>
-										<img class="img-cover lazyload" data-src="imgs/default.png" alt="Imagem 02">
-									</figure>
-								</button>
-							</div>
-							<div class="item col">
-								<button data-src="imgs/default.png" data-fancybox="gallery">
-									<figure>
-										<img class="img-cover lazyload" data-src="imgs/default.png" alt="Imagem 03">
-									</figure>
-								</button>
-							</div>
-							<div class="item col">
-								<button data-src="imgs/default.png" data-fancybox="gallery">
-									<figure>
-										<img class="img-cover lazyload" data-src="imgs/default.png" alt="Imagem 04">
-									</figure>
-								</button>
-							</div>
-							<div class="item col">
-								<button data-src="imgs/default.png" data-fancybox="gallery">
-									<figure>
-										<img class="img-cover lazyload" data-src="imgs/default.png" alt="Imagem 05">
-									</figure>
-								</button>
-							</div>
-							<div class="item col">
-								<button data-src="imgs/default.png" data-fancybox="gallery">
-									<figure>
-										<img class="img-cover lazyload" data-src="imgs/default.png" alt="Imagem 06">
-									</figure>
-								</button>
-							</div>
-							<div class="item col">
-								<button data-src="imgs/default.png" data-fancybox="gallery">
-									<figure>
-										<img class="img-cover lazyload" data-src="imgs/default.png" alt="Imagem 07">
-									</figure>
-								</button>
-							</div>
-							<div class="item col">
-								<button data-src="imgs/default.png" data-fancybox="gallery">
-									<figure>
-										<img class="img-cover lazyload" data-src="imgs/default.png" alt="Imagem 08">
-									</figure>
-								</button>
-							</div>
-						</div>
+<section>
+	<div class="breadcrumb">
+		<span>quem somos</span>
+	</div>
+</section>
+<main id="page" tabindex="-1">
+	<!-- <div class="container">
+		<section class="page">
+			<article>
+				<div class="row">
+					<div class="col-12 sec">
+						<img src="./imgs/people.png" alt="">
+						<h5>Especialistas em selecionar
+							apenas os melhores imóveis</h5>
+						<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+							ut labore
+							et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+							nisi ut
+							aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+							esse
+							cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+							in
+							culpa qui </p>
 					</div>
-				</article>
-			</section>
-			<section class="btn-default text-right">
-				<div class="container">
-					<a href="#">
-					    <i class="fas fa-angle-left"></i>
-					    <span>Voltar</span>
-					</a>
+					<div class="col-12">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+							labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+							laboris nisi ut aliquip ex ea </p>
+					</div>
+					<div class="col-12">
+						<h5>missão</h5>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+							labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+							laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+							voluptate velit </p>
+						<h5>valores</h5>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+							labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+							laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+							voluptate velit </p>
+					</div>
+					<div class="col-12">Sec 4</div>
 				</div>
-				<br/><br/>
-			</section>
-		</div>
-	</main>
+			</article>
+		</section>
+		<section class="btn-default text-right">
+			<div class="container">
+				<a href="#">
+					<i class="fas fa-angle-left"></i>
+					<span>Voltar</span>
+				</a>
+			</div>
+			<br /><br />
+		</section>
+	</div> -->
+	<div class="row">
+		<div class="col-12 sec s-1 row">
+			<div class="col-12 col-lg-6 thumbnail ">
+				<img src="./imgs/people.png" alt="people" class="d-none d-sm-block lazyload">
+				<img src="./imgs/people-m.png" alt="people" class="d-block d-sm-none lazyload">
 
+			</div>
+			<div class="col-12 col-lg-6 description">
+				<div class="description-text">
+					<h5>Especialistas em selecionar</h5>
+					<h5>apenas os melhores imóveis</h5>
+					<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+						ut labore
+						et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+						nisi ut
+						aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+						esse
+						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+						in
+						culpa qui </p>
+				</div>
+			</div>
+
+		</div>
+		<div class="col-12 sec s-2 row">
+			<div class="col-12 col-sm-6 thumbnail justify-content-end d-none d-sm-flex">
+				<img src="./imgs/bee2.png" alt="bee2" class=" lazyload">
+			</div>
+			<div class="col-12 col-sm-6 description d-flex align-middle">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut
+					labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+					laboris nisi ut aliquip ex ea </p>
+			</div>
+
+
+		</div>
+		<div class="col-12 sec s-3 row">
+
+			<div class="description col-12 col-lg-6">
+				<div class="description-text">
+					<h5>missão</h5>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+						labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+						laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+						voluptate velit </p>
+					<h5>visão</h5>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+						labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+						laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+						voluptate velit </p>
+					<h5>valores</h5>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+						labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+						laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+						voluptate velit </p>
+				</div>
+			</div>
+			<div class="thumbnail col-12 col-lg-6 p-0">
+				<!-- <img class="" src="./imgs/orange_bee.png" alt="orange_bee"> -->
+				<img src="./imgs/orange-rec.png" alt="orange-rec" class="bg d-none d-lg-block lazyload">
+				<img src="./imgs/orange-rec-m.png" alt="orange-rec-," class="bg-m d-block d-lg-none lazyload">
+				<img src="./imgs/bee_oct.png" alt="bee_oct" class="bee lazyload">
+
+			</div>
+		</div>
+		<div class="col-12  sec s-4 d-flex justify-content-center">
+			<div class="row col-12 col-lg-6">
+				<p class="header col-12 ">Conheça os imóveis da Jataí</p>
+				<div class="col-12 col-sm-6">
+					<div class="item">
+						<a href="#item-"><img src="./imgs/items/item-0.png" alt="item-0" class="lazyload">
+							<p>Cidade</p>
+						</a>
+					</div>
+				</div>
+				<div class="col-12 col-sm-6">
+					<div class="item">
+						<a href="#item-"><img src="./imgs/items/item-1.png" alt="item-1" class="lazyload">
+							<p>Industrial</p>
+						</a>
+					</div>
+				</div>
+				<div class="col-12 col-sm-6">
+					<div class="item">
+						<a href="#item-"><img src="./imgs/items/item-2.png" alt="item-2" class="lazyload">
+							<p>Praia</p>
+						</a>
+					</div>
+				</div>
+				<div class="col-12 col-sm-6">
+					<div class="item">
+						<a href="#item-"><img src="./imgs/items/item-3.png" alt="item-3" class="lazyload">
+							<p>Campo</p>
+						</a>
+					</div>
+				</div>
+				<div class="col-12 col-sm-6">
+					<div class="item">
+						<a href="#item-"><img src="./imgs/items/item-4.png" alt="item-4" class="lazyload">
+							<p>Comércio</p>
+						</a>
+					</div>
+				</div>
+				<div class="col-12 col-sm-6">
+					<div class="item">
+						<a href="#item-"><img src="./imgs/items/item-5.png" alt="item-5" class="lazyload">
+							<p>+ VER TODOS</p>
+						</a>
+					</div>
+				</div>
+			</div>
+
+
+		</div>
+	</div>
+</main>
+<?php include('realty-search.php'); ?>
 <?php include('footer.php'); ?>
